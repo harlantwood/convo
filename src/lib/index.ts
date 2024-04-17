cf6dc576e87ff817effba3313e4706c179409a6f
Harlan Wood <code@harlantwood.net>
2024-04-16 15:10:24 -1000

    Prettier: no semi

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,10 @@
 import { playAudio as browserPlayAudio } from './textToSpeech/browser/playAudio'
 import { response as serverOpenAiResponse } from './textToSpeech/server/openai'
 
-
 export const tts = {
-  browser: {playAudio: browserPlayAudio},
-  server: {openai: {response: serverOpenAiResponse}}
+	browser: { playAudio: browserPlayAudio },
+	server: { openai: { response: serverOpenAiResponse } },
 }
 
 // TODO fold into above API
-export { speechToText} from './speechToText'
+export { speechToText } from './speechToText'
--- src/lib/textToSpeech/server/openai.ts
+++ src/lib/textToSpeech/server/openai.ts
@@ -1,21 +1,21 @@
 import OpenAI from 'openai'
 import { Buffer } from 'buffer/'
 
-export async function response( input, {model, voice, apiKey}) {
-  const openai = new OpenAI({ apiKey })
+export async function response(input, { model, voice, apiKey }) {
+	const openai = new OpenAI({ apiKey })
 
-  const mp3 = await openai.audio.speech.create({
-    model: 'tts-1',
-    voice: 'alloy',
-    input,
-  })
+	const mp3 = await openai.audio.speech.create({
+		model: 'tts-1',
+		voice: 'alloy',
+		input,
+	})
 
-  const buffer = Buffer.from(await mp3.arrayBuffer())
+	const buffer = Buffer.from(await mp3.arrayBuffer())
 
-  return new Response(buffer, {
-    status: 200,
-    headers: {
-      'Content-Type': 'audio/mpeg',
-    },
-  })
+	return new Response(buffer, {
+		status: 200,
+		headers: {
+			'Content-Type': 'audio/mpeg',
+		},
+	})
 }
